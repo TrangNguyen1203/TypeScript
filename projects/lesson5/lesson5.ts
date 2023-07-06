16b4d665ff25f5900b111229b5cd2da9014e162a
TrangNguyen1203 <nguyenthitrang.tte@gmail.com>
2023-07-06 12:37:19 +0700

    object

---
+++ projects/lesson5/lesson5.ts
@@ -0,0 +1,21 @@
+let mine1: {} = {
+    name1: "Trang",
+    fullname: "Nguyễn Thị Trang",
+    age: 18
+}
+
+// OR
+let mine2: {
+    name1: string,
+    fullname: string,
+    age: number
+} = {
+    name1: "Trang",
+    fullname: "Nguyễn Thị Trang",
+    age: 18
+}
+
+mine2.age = 19
+
+console.log(">>>Check mine1: ", mine1);
+console.log(">>>Check mine2: ", mine2);
