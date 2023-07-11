50a2e6130f29f13c074437c2d051675eeac7e6d6
TrangNguyen1203 <nguyenthitrang.tte@gmail.com>
2023-07-11 09:09:47 +0700

    lesson 8, lesson 9

---
+++ projects/lesson8/lesson8.js
@@ -0,0 +1,5 @@
+let name1 = 'Trang' //STRING
+// có kiểu dữ liệu động
+name1 = 35;//number
+name1 = true;//boolean
+
---
+++ projects/lesson8/lesson8.ts
@@ -0,0 +1,7 @@
+// kiểu dữ liệu giúp bỏ qua việc check type của biến anytype4
+let name2: any = 'Trang'
+name2 = true;
+name2 = 20
+console.log(">>>check name2: ", name2);
+
+
